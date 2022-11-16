5827f4c222075485c0f1d8f3c4224dc37e838e73
jaredtech <hk_rjc@163.com>
2022-11-16 11:38:13 +0800

    Add a interface class to convert response data to json string

--- flutter_loggy_dio/lib/flutter_loggy_dio/dio_loggy_convert.dart
+++ flutter_loggy_dio/lib/flutter_loggy_dio/dio_loggy_convert.dart
@@ -1,13 +1,13 @@
 part of flutter_loggy_dio;
-///Convert dio response data to a string.
-///By default, use [toString] method to convert the response data.
-///Or, maybe you want to use [JsonEncoder] to encode your data before loggy log, like this:
+///Convert dio response data to a json string before loggy log.
+///By default, using [toString] method to convert the response data.
+///Or, use a [JsonEncoder] to encode a response data , like this:
 ///```dart
 /// json.decode('["foo", { "bar": 499 }]');
 ///```
-///Because, in some cases, you may get a response data like this:
+///Because, in some cases, a response data has no quotes, like this:
 ///```dart
-/// '[foo, { name: myName }]';
+/// '[foo, { name: jj }]';
 ///```
 class DioLoggyConvert {
   String convertData(dynamic data){
